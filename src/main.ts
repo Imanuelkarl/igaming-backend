db50bc91e1717246c288d498ef7efb55ce9619d9
Imanuelkarl <appdeveloperemmanuel@gmail.com>
2025-08-06 11:59:33 +0100

    cors-muliple origins

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,20 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = [
+  'http://localhost:5173',
+  'https://your-netlify-app.netlify.app',
+  'https://imanuelkarl.gitub.io'
+];
   // ✅ Enable CORS
   app.enableCors({
-    origin: ['http://localhost:5173','imanuelkarl.github.io', 'https://igame-projects.netlify.app'], // 👈 frontend URL (React, Angular, etc.)
+    origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }, // 👈 frontend URL (React, Angular, etc.)
     credentials: true,               // 👈 if you're using cookies or auth headers
   });
 
